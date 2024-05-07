4f480a65672d6ddbde33fdae35783d00107887ec
JouniGitti <kalaksi@gmail.com>
2024-05-07 14:33:19 +0300

    Sorter now refactored to be an abstract class in stead of an interface - implementations also refactored

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,38 @@
-import { Sorter } from './Sorter';
+
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection'; 
 import { LinkedList } from './LinkedList';
-import { link } from 'fs';
 
-//const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+const numbersCollection = new NumbersCollection([10, 3, -5, 0])
+numbersCollection.sort();
+console.log(numbersCollection.data);
 
+// previous interface implementation:
 //const sorter = new Sorter(numbersCollection);
 //sorter.sort();
 //console.log(numbersCollection.data)
 
-/*const charactersCollection = new CharactersCollection('XECPoaayb')
-console.log(charactersCollection)
-const sorter = new Sorter(charactersCollection);
+const charactersCollection = new CharactersCollection('XECPoasbayb')
+charactersCollection.sort();
+console.log(charactersCollection.data);
+
+// previous interface implementation:
+/* const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(charactersCollection.data);*/
 
 const linkedList = new LinkedList;
 linkedList.add(300)
 linkedList.add(-10)
-linkedList.add(-7)
+linkedList.add(-17)
 linkedList.add(8)
-
-const sorter = new Sorter(linkedList);
-sorter.sort();
+linkedList.sort();
 linkedList.print();
+
+// previous interface implementation:
+/*const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();*/
+
 
 
